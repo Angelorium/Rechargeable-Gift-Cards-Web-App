1a2b895eb9853c668e05a587625b6cf095e42bc5
Angelorium <angelo.barbera01@gmail.com>
2023-05-08 12:53:23 +0200

    initial commit

---
+++ src/main/java/com/project/wsda/WsdaApplication.java
@@ -0,0 +1,13 @@
+package com.project.wsda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/wsda/WsdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wsda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
